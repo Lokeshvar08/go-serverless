bd2e7e3e847a9d9ba2f33135bfb7aa313ec51675
Lokeshvar S <lokeshvarsakthi@gmail.com>
2021-12-19 15:18:52 +0530

    kitchen panel nav data

--- src/main/java/com/restaurant/server/restaurantservermanager/controller/KitchenController.java
+++ src/main/java/com/restaurant/server/restaurantservermanager/controller/KitchenController.java
@@ -2,6 +2,7 @@ package com.restaurant.server.restaurantservermanager.controller;
 
 import com.restaurant.server.restaurantservermanager.model.User;
 import com.restaurant.server.restaurantservermanager.security.AuthenticatedUser;
+import com.restaurant.server.restaurantservermanager.service.forms.user.AuthenticatedUserClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -25,13 +26,46 @@ public class KitchenController {
     }
 
     @GetMapping("/manage-food")
-    public String home() {
-        return "kitchen/manage-food";
+    public ModelAndView home(ModelAndView mv) {
+
+        try {
+            mv.setViewName("kitchen/manage-food");
+            User user = authenticatedUser.getAuthenticatedUserObject();
+            mv.addObject("user", new AuthenticatedUserClient(
+                    user.getId(),
+                    user.getUsername(),
+                    user.getName(),
+                    user.getRole().toString(),
+                    user.getRestaurant().getName(),
+                    user.getRestaurant().getId()
+            ));
+            return mv;
+        }catch (Exception e) {
+            mv.setViewName("auth/login");
+            mv.addObject("error", "invalid");
+            return mv;
+        }
     }
 
     @GetMapping("/orders")
-    public String getOrder() {
-        return "kitchen/orders";
+    public ModelAndView getOrder(ModelAndView mv) {
+        try {
+            mv.setViewName("kitchen/orders");
+            User user = authenticatedUser.getAuthenticatedUserObject();
+            mv.addObject("user", new AuthenticatedUserClient(
+                    user.getId(),
+                    user.getUsername(),
+                    user.getName(),
+                    user.getRole().toString(),
+                    user.getRestaurant().getName(),
+                    user.getRestaurant().getId()
+            ));
+            return mv;
+        }catch (Exception e) {
+            mv.setViewName("auth/login");
+            mv.addObject("error", "invalid");
+            return mv;
+        }
     }
 
 }

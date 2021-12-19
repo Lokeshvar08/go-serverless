eda0877f905a6b5e32badde6df9ddeb7b52bcf81
Lokeshvar S <lokeshvarsakthi@gmail.com>
2021-12-19 13:58:38 +0530

    manager navbar details bug fix

--- src/main/java/com/restaurant/server/restaurantservermanager/service/forms/user/AuthenticatedUserClient.java
+++ src/main/java/com/restaurant/server/restaurantservermanager/service/forms/user/AuthenticatedUserClient.java
@@ -14,4 +14,44 @@ public class AuthenticatedUserClient {
         this.role = role;
         this.restaurant = restaurant;
     }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getRestaurant() {
+        return restaurant;
+    }
+
+    public void setRestaurant(String restaurant) {
+        this.restaurant = restaurant;
+    }
 }
